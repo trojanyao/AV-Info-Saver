26cc05ec7592698e9630018de7b4be4e56b362af
Trojan Yao <ytj1996@sina.com>
2022-03-12 20:39:00 +0800

    feat: 番号格式标准化新增适配
    适配无码厂商纯数字番号

--- src/utils/codify.ts
+++ src/utils/codify.ts
@@ -1,13 +1,19 @@
 // 品番 标准化
 export function codify(code: string) {
-    // 番号数字位
-    let codeNum = code.match(/\d+/)
-    let codeCap = code.match(/[A-Z,a-z]+/)
-    let newCodeNum
-    // 处理番号数字位大于3，有多个0的情况
-    if (codeNum[0] && codeNum[0].length > 3) {
-        newCodeNum = codeNum[0].slice(-3)
-        code = code.replace(codeNum[0], newCodeNum)
+    if (/[a-z,A-Z]/.test(code)) {
+        // 有字母的番号
+        // 番号数字位
+        let codeNum = code.match(/\d+/)
+        let codeCap = code.match(/[A-Z,a-z]+/)
+        let newCodeNum
+        // 处理番号数字位大于3，有多个0的情况
+        if (codeNum[0] && codeNum[0].length > 3) {
+            newCodeNum = codeNum[0].slice(-3)
+            code = code.replace(codeNum[0], newCodeNum)
+        }
+        return `${codeCap}-${codeNum}`.toUpperCase();
+    } else {
+        // 纯数字番号
+        return code
     }
-    return `${codeCap}-${codeNum}`.toUpperCase();
 }
