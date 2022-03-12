5803302ca9ff65696a03e06285993ce27778cfd8
Trojan Yao <ytj1996@sina.com>
2022-03-12 18:50:31 +0800

    feat: 核心功能
    1. 创建页面按钮；
    2. 增加按钮业务逻辑。

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,49 @@
-import { get } from './utils'
-import './style/main.less'
-import { add } from './example'
+// import { get } from './utils'
+// import './style/main.less'
+// import { add } from './example'
+import { createBtn } from './create-btn'
+import { final } from './utils/final'
+import { Madonna } from './makers/madonna'
+import { NA } from './makers/naughty-america'
 
-async function main () {
-  console.log('script start')
-  console.log(`1 + 2 = ${add(1, 2)}`)
-  const res = await get<{ uuid: string }>('/uuid', {
-    baseURL: 'https://httpbin.org',
-  })
-  console.log(`uuid: ${res.data.uuid}`)
+async function main() {
+	// 创建按钮
+	let a = createBtn() 
+
+	// 核心功能
+	let domain = document.domain, url = document.URL;
+	let av: any = {};
+	console.log('域名', domain, url);
+	async function trySwitch() {
+		switch (domain) {
+			case 'www.madonna-av.com': av = Madonna(url); break;
+			case 'www.naughtyamerica.com':
+				try {
+					av = await NA(url)
+				}catch(e) {
+
+				}
+				break;
+		}
+	}
+	await trySwitch()
+
+	if (av) {
+		console.log('AV对象', av)
+		try {
+			a.download = await final(av)
+			a.href = av.imgUrl
+		} catch(e) {
+
+		}
+	}
+
+	// if (autoSave === 'yes') {
+	// 	a.click();
+	// }
+
 }
 
 main().catch((e) => {
-  console.log(e)
+	console.log(e)
 })
