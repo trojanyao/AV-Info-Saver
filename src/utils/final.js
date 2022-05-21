3d446802a0b404daca162975895a293e7d591367
Trojan Yao <ytj1996@gmail.com>
2022-05-21 10:58:50 +0800

    fix: 修复「不识别作品标题头尾是否包含演员名」的问题

--- src/utils/final.js
+++ src/utils/final.js
@@ -35,13 +35,15 @@ export async function final(avObj) {
             // 去头尾演员名
             let startAct = new RegExp('^' + av.actress, 'g')
             let endAct = new RegExp(av.actress + '$', 'g')
-            console.log('头尾是否包含演员名', startAct.test(av.workName))
-            if (startAct.test(av.workName) || endAct.test(av.workName)) {
+            let temp = startAct.test(av.workName) || endAct.test(av.workName)
+            console.log('头尾是否包含演员名', temp)
+            if (temp) {
+                console.log('演员名字符串', av.actress)
                 av.workName = av.workName.replace(av.actress, '')
             }
             // 头尾去空格
             av.workName = av.workName.trim()
-            console.log('标题', av.workName)
+            console.log('去头尾演员名后的标题', av.workName)
         }
 
         if (av && av.seriesName) {
