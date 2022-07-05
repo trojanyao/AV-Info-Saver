78944d4e5f4aef4fec40f4552442c98861bb3129
TROJAN <ytj1996@sina.com>
2022-07-05 10:51:41 +0800

    feat: 调整番号处理规则

--- src/utils/codify.js
+++ src/utils/codify.js
@@ -1,7 +1,11 @@
 // 品番 标准化
 export function codify(code) {
+    console.log('处理前番号', code)
     if (/n\d+/.test(code)) {
         // 東京熱番号
+        return code
+    } else if (/-/.test(code)) {
+        // 包含连字符的番号无需处理
         return code
     } else if (/[a-z,A-Z]/.test(code)) {
         // 有字母的番号
@@ -14,7 +18,7 @@ export function codify(code) {
             newCodeNum = codeNum[0].slice(-3)
             code = code.replace(codeNum[0], newCodeNum)
         }
-        return `${codeCap}-${codeNum}`.toUpperCase();
+        return `${codeCap}-${codeNum}`.toUpperCase()
     } else {
         // 纯数字番号
         return code
