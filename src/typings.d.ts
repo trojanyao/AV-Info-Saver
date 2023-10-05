781167401ae84dcbf4b351df5dd870af517d8c2f
Trojan Yao <ytj1996@sina.com>
2023-10-05 21:37:13 +0800

    feat(typings.d.ts): add "AVWork" interface

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,1 +1,11 @@
-declare module '*.less';
+declare module "*.less";
+
+export interface AVWork {
+  makerName: string; // 厂商名
+  workName: string; // 作品名
+  seriesName?: string; // 系列名
+  date: string; // 发布日期
+  actress: string[]; // 女优
+  code: string; // 番号
+  imgUrl: string; // 封面图片地址
+}
