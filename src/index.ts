8a011999f449b2086174009ae10d15d4226602e5
Trojan Yao <ytj1996@sina.com>
2023-10-05 21:28:07 +0800

    refactor(index.ts): code refactor

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { createBtn } from "./create-btn";
 import final from "./utils/final";
+
+/* ===== Makers ===== */
 import CA from "./makers/ca_group";
 import { NA } from "./makers/naughty-america";
 import { OnePondo } from "./makers/1pondo";
@@ -11,100 +13,95 @@ import SOD from "./makers/sod";
 import MGS from "./makers/mgstage";
 import Mousouzuku from "./makers/mousouzoku";
 
-async function main() {
-  // 创建按钮
-  let a = createBtn();
+window.onload = async () => {
+  await main();
+};
 
-  // 核心功能
+async function main() {
+  const a = createBtn(); // 创建按钮
+
   const domain = document.domain;
   const url = document.URL;
   let av: any = {};
 
-  async function trySwitch() {
-    switch (domain) {
-      // ----- CA 集团厂商 -----
-      case "madonna-av.com":
-      case "s1s1s1.com":
-      case "moodyz.com":
-      case "honnaka.jp":
-      case "ideapocket.com":
-      case "attackers.net":
-      case "premium-beauty.com":
-      case "mvg.jp":
-        av = await CA(url);
-        break;
-      case "www.naughtyamerica.com":
-        try {
-          av = await NA(url);
-        } catch {}
-        break;
-      case "www.1pondo.tv":
-        try {
-          av = await OnePondo(url);
-        } catch {}
-        break;
-      case "www.prestige-av.com":
-        try {
-          av = await Prestige(url);
-        } catch {}
-        break;
-      case "my.tokyo-hot.com":
-        try {
-          av = await TokyoHot(url);
-        } catch {}
-        break;
-      case "www.brazzers.com":
-        try {
-          setTimeout(async () => {
-            av = await Brazzers(url);
-            if (av) {
-              try {
-                a.download = await final(av);
-                a.href = av.imgUrl;
-              } catch (e) {}
-            }
-          }, 2000);
-        } catch (e) {}
-        break;
-      case "www.caribbeancom.com":
-        try {
-          av = await Caribbean(url);
-        } catch {}
-        break;
-      case "ec.sod.co.jp":
-        try {
-          av = await SOD(url);
-        } catch {}
-        break;
-      case "www.mgstage.com":
-        try {
-          av = await MGS(url);
-        } catch {}
-        break;
-      case "www.mousouzoku-av.com":
-        try {
-          av = await Mousouzuku(url);
-        } catch {}
-        break;
-    }
-  }
-
-  trySwitch().then(async () => {
+  trySwitch(domain, url, av, a).then(async () => {
     if (av) {
-      try {
-        a.download = await final(av);
-        a.href = av.imgUrl;
+      a.download = await final(av);
+      a.href = av.imgUrl;
 
-        if (localStorage.getItem("autoSave") === "yes") {
-          a.click();
-        }
-      } catch (e) {}
+      // 自动保存开启
+      if (localStorage.getItem("autoSave") === "yes") {
+        a.click();
+      }
     }
   });
 }
 
-window.onload = async () => {
-  try {
-    await main();
-  } catch (e) {}
-};
+/**
+ * 根据不同厂商，使用不同的处理脚本，设置不同的按钮内容
+ * @param domain 网站域名
+ * @param url 作品页URL地址
+ * @param av AV对象
+ * @param a 生成的按钮
+ */
+async function trySwitch(domain: string, url: string, av: any, a: any) {
+  switch (domain) {
+    /* ========== 无码 ========== */
+    case "www.1pondo.tv":
+      av = await OnePondo(url);
+      break;
+    case "www.caribbeancom.com":
+      av = await Caribbean(url);
+      break;
+    case "my.tokyo-hot.com":
+      av = await TokyoHot(url);
+      break;
+
+    /* ========== 有码 ========== */
+    // CA 集团
+    case "madonna-av.com":
+    case "s1s1s1.com":
+    case "moodyz.com":
+    case "honnaka.jp":
+    case "ideapocket.com":
+    case "attackers.net":
+    case "premium-beauty.com":
+    case "mvg.jp":
+      av = await CA(url);
+      break;
+    case "www.mousouzoku-av.com":
+      av = await Mousouzuku(url);
+      break;
+
+    // SOD 集团
+    case "ec.sod.co.jp":
+      av = await SOD(url);
+      break;
+
+    // Prestige 集团
+    case "www.prestige-av.com":
+      av = await Prestige(url);
+      break;
+
+    /* ========== 欧美 ========== */
+    case "www.brazzers.com":
+      setTimeout(async () => {
+        av = await Brazzers(url);
+        if (av) {
+          try {
+            a.download = await final(av);
+            a.href = av.imgUrl;
+          } catch (e) {}
+        }
+      }, 2000);
+      break;
+    case "www.naughtyamerica.com":
+      av = await NA(url);
+      break;
+
+    /* ========== 素人 ========== */
+    case "www.mgstage.com":
+      av = await MGS(url);
+      break;
+  }
+}
