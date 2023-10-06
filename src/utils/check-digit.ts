6e24263239cfce1b07fe86361532dd0ccf6dfe1e
Trojan Yao <ytj1996@sina.com>
2023-10-06 17:40:25 +0800

    refactor(check-digit.js): change to typescript

---
+++ src/utils/check-digit.ts
@@ -0,0 +1,24 @@
+/**
+ * 检测系列作品的标题中是否包含纯数字编号
+ *
+ * @param {string} workName 作品名
+ * @param {string} seriesName 系列名
+ * @returns { string | undefined } 检测出的纯数字编号或 undefined（未检测出）
+ */
+export function checkDigit(workName: string, seriesName: string): string | undefined {
+  const seriesNameNoSpace = seriesName.replaceAll(' ', '')
+
+  /* 有纯数字编号也肯定是在标题中的系列名后跟着纯数字 */
+  const reg = new RegExp(`\\s*${seriesNameNoSpace}\\s*(\\d+)`, 'g')
+  const matches = [...workName.replaceAll(' ', '').matchAll(reg)]
+
+  for (const match of matches) {
+    if (match.length >= 2) {
+      // 说明匹配到了捕获组，将其返回（系列编号）
+      return match[1]
+    }
+  }
+
+  // 不包含纯数字
+  return undefined
+}
