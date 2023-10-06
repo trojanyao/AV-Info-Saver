1ed1fe8e59130133c76f3dc47c614c40347e589d
Trojan Yao <ytj1996@sina.com>
2023-10-06 16:56:02 +0800

    refactor(final.js): change to typescript

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,12 +1,14 @@
-declare module "*.less";
+declare module "*.less"
 
 export interface AVWork {
-  makerName: string; // 厂商名
-  workName: string; // 作品名
-  seriesName?: string; // 系列名
-  date: string; // 发布日期
-  actress: string[]; // 女优
-  code: string; // 番号
-  imgUrl: string; // 封面图片地址
-  duration?: string; // 时长
+  makerName: string // 厂商名
+  workName: string // 作品名
+  seriesName?: string // 系列名
+  date: string // 发布日期
+  actress: string | string[] // 女优
+  actressRealName?: string // 素人女优的真实姓名
+  code: string // 番号
+  imgUrl: string // 封面图片地址
+  duration?: string // 时长
+  resolution?: string[] // 清晰度
 }
