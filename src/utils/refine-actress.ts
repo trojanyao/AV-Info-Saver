5cbaeac442dfa11ecf3b1b7ee8fde25e599a6563
Trojan Yao <ytj1996@sina.com>
2023-10-06 16:56:23 +0800

    refactor(refine-actress.js): change to typescript

---
+++ src/utils/refine-actress.ts
@@ -0,0 +1,25 @@
+import { AVWork } from '../typings'
+
+/**
+ * 美化演员列表
+ * - 剔除头尾空格
+ * - 剔除名字内空格（日本演员）
+ *
+ * @param {AVWork} av 未处理的 AV 对象
+ * @returns {AVWork} 处理后的 AV 对象
+ */
+export function refineActress(av: AVWork) {
+  av.actress = (av.actress as string[]).map((a) => {
+    // 先剔除头尾空格
+    let newA = a.trim()
+
+    // 剔除内部空格（仅作用于非英文名演员、非素人演员）
+    if (!newA.match(/[a-zA-Z]+/g) && !av.actressRealName) {
+      newA = newA.replaceAll(/\s/g, '')
+    }
+    return newA
+  })
+
+  av.actress = (av.actress as string[]).join(' ')
+  return av
+}
