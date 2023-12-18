4168f661726c41c7a28943b7b581937fa559833e
TROJAN <ytj1996@gmail.com>
2023-12-18 16:59:30 +0800

    feat: 独立封装有码厂商 finalName 逻辑

--- src/utils/final.ts
+++ src/utils/final.ts
@@ -105,3 +105,59 @@ export default async function final(avObj: AVWork): Promise<string> {
   )} - ${av.workName}${av.duration ? `[${av.duration}; ${av.resolution.join(', ')}]` : ''}.jpg`
   return finalName
 }
+
+/**
+ * 是否需要移除系列名后的空格
+ */
+function seriesEndSpace(list: string[], name: string) {
+  return list?.includes(name) ? '' : ' '
+}
+
+/**
+ * 拼接最终文件名（有码）
+ */
+export function finalCensored(av: AVWork, digitFirstSeriesList: string[] = [], noSeriesEndSpaceList: string[] = []) {
+  let finalName: string
+
+  /* === 1. 处理演员名 === */
+  av = refineActress(av)
+  const actressString = av.actress.join(' ')
+
+  /* === 2. 处理标题 === */
+  av = refineTitle(av)
+
+  /* === 3. 处理番号 === */
+  av.code = codify(av.code)
+
+  /* === 3. 处理系列名 === */
+  if (av && av.seriesName) {
+    /* 系列作品 */
+    av.seriesName = av.seriesName.trim()
+
+    const numType1 = checkIndicator(av.workName) // 标识 + 编号
+    const numType2 = checkDigit(av.workName, av.seriesName) // 纯数字编号
+
+    if (numType1 || (!numType1 && numType2)) {
+      /* 包含编号标识 || 不包含编号标识但包含纯数字 */
+      const digitFirstSeries = digitFirstSeriesList.find((x) => av.seriesName === x)
+
+      if (digitFirstSeries) {
+        // 系列编号在前：【厂商】系列名 编号（日期）演员名（番号）[时长]
+        finalName = `${av.seriesName}${seriesEndSpace(noSeriesEndSpaceList, av?.seriesName)}${
+          numType1 || numType2
+        }（${datify(av.date)}）${actressString}（${av.code}）`
+      } else {
+        // 系列编号在后：【厂商】系列名（日期）编号（番号）演员名 [时长]
+        finalName = `${av.seriesName}（${datify(av.date)}）${numType1 || numType2}（${av.code}）${actressString}`
+      }
+    } else {
+      /* 不含编号标识：【厂商】系列名（日期）演员名（番号）[时长] */
+      finalName = `${av.seriesName}（${datify(av.date)}）${actressString}（${av.code}）`
+    }
+  } else {
+    /* 非系列作品：【厂商】（日期）演员（番号）作品名 [时长] */
+    finalName = `（${datify(av.date)}）${actressString}（${codify(av.code)}）${av.workName}`
+  }
+
+  return `【${av.makerName}】${finalName}.jpg`
+}
